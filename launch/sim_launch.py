7a50cf152881b52632ebd02b65a8d69e2181938d
truongdang <quangtruonglf@gmail.com>
2025-03-13 17:24:28 +0700

    lidar in rviz, launch rviz

--- launch/sim_launch.py
+++ launch/sim_launch.py
@@ -3,9 +3,8 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
 
 from launch_ros.actions import Node
 
@@ -13,11 +12,12 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
         package_name = 'slam_nav' 
+        package_share_directory = get_package_share_directory(package_name)
 
         # Robot state publisher
         robot_state_publisher = IncludeLaunchDescription(
                         PythonLaunchDescriptionSource([os.path.join(
-                        get_package_share_directory(package_name),'launch','robot_state_publisher.launch.py'
+                        package_share_directory,'launch','robot_state_publisher.launch.py'
                         )]))
 
         # Gazebo Sim
@@ -31,16 +31,39 @@ def generate_launch_description():
                         launch_arguments={'gz_args': ['-r ', world]}.items())
 
         # Spawm robot
-        spawn = Node(package='ros_gz_sim', executable='create',
-                                parameters=[{
-                                        'name': 'my_robot_sim',
-                                        'topic': 'robot_description',
-                                        'z': 0.5,}],
-                                        output='screen')
+        spawn = Node(
+                package='ros_gz_sim', 
+                executable='create',
+                parameters=[{
+                        'name': 'my_robot_sim',
+                        'topic': 'robot_description',
+                        'z': 0.5,}],
+                        output='screen')
+        # ROS - Gz bridge
+        bridge_params = os.path.join(
+                                package_share_directory,
+                                'config',
+                                'gz_bridge.yaml')
+        bridge = Node(
+                        package='ros_gz_bridge',
+                        executable='parameter_bridge',
+                        arguments=[
+                              '--ros-args',
+                              '-p',
+                              f'config_file:={bridge_params}'],
+                              output='screen')
+        
+        # Rviz
+        rviz = Node(
+                package='rviz2',
+                executable='rviz2',
+                arguments=['-d',os.path.join(package_share_directory,'config','rviz_config.rviz')])
 
         # Launch
         return LaunchDescription([
                 robot_state_publisher,
                 gazebo,
-                spawn
+                spawn,
+                bridge,
+                rviz
         ])
