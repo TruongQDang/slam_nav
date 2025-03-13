7f2b90192438980386caa69203e279932ea7aae6
truongdang <quangtruonglf@gmail.com>
2025-03-13 13:32:28 +0700

    rough urdf model, robot state publisher

--- launch/robotstatepubliser_launch.py
+++ launch/robotstatepubliser_launch.py
@@ -3,11 +3,13 @@ import xacro
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
 
 def generate_launch_description():
         # Package name and Path to xacro file
         pkg_name = 'slam_nav'
-        file_subpath = 'description/robot_urdf.xacro'
+        file_subpath = 'description/robot.urdf.xacro'
 
         # Parse robot description from xacro
         xacro_file = os.path.join(
@@ -17,15 +19,21 @@ def generate_launch_description():
         robot_description = xacro.process_file(xacro_file).toxml()
 
         # Robot state publisher
+        use_sim_time = LaunchConfiguration('use_sim_time')
+        params = {'use_sim_time': use_sim_time, 'robot_description': robot_description}
         node_robot_state_publisher = Node(
                 package='robot_state_publisher',
                 executable='robot_state_publisher',
                 name="robot_state_publisher",
-                output='both',
-                parameters=[{'robot_description': robot_description}]
+                output='screen',
+                parameters=[params],
         )
 
         # Run the node
         return LaunchDescription([
+                DeclareLaunchArgument(
+                        'use_sim_time',
+                        default_value='true',
+                        description='Use sim time if true'),
                 node_robot_state_publisher
         ])
